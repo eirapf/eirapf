99d6354199d38b46f937aaecdb1767b6f6e457b6
eirapf <eirapf@gmail.com>
2025-01-27 21:58:11 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,199 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-aeroport_01",
+      "name": "Aeroport_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19722584907099616,
+          "pitch": 0.4437953242785895,
+          "rotation": 0,
+          "target": "4-aeroport_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-aeroport_03",
+      "name": "Aeroport_03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6272968434015418,
+          "pitch": 0.5258002151573322,
+          "rotation": 0,
+          "target": "2-aeroport_04"
+        },
+        {
+          "yaw": 0.6466921425177539,
+          "pitch": -0.09189487410355213,
+          "rotation": 0,
+          "target": "4-aeroport_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aeroport_04",
+      "name": "Aeroport_04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.3302611428417581,
+        "pitch": 0.08905282914731849,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0104759093796218,
+          "pitch": 0.6178725912040974,
+          "rotation": 0,
+          "target": "1-aeroport_03"
+        },
+        {
+          "yaw": -0.25475579088845635,
+          "pitch": 0.5879088002255006,
+          "rotation": 0,
+          "target": "3-aeroport_05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-aeroport_05",
+      "name": "Aeroport_05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.006010377099615383,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2951901442560274,
+          "pitch": -0.12406421488642039,
+          "rotation": 0,
+          "target": "2-aeroport_04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-aeroport_02",
+      "name": "Aeroport_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.5209136536721459,
+        "pitch": 0.10018443279072997,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07898503436591042,
+          "pitch": 0.606455060846999,
+          "rotation": 0,
+          "target": "1-aeroport_03"
+        },
+        {
+          "yaw": 2.2966603346617696,
+          "pitch": 0.5423947745854178,
+          "rotation": 0,
+          "target": "0-aeroport_01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Recorregut Virtual",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
